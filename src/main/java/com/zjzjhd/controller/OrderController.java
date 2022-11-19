3459dee0d1afe634d59c58838b639f0c4bdb04d8
zjzjhd <zjzjhd@hotmail.com>
2022-11-19 20:39:55 +0800

    修改购物车界面

--- src/main/java/com/zjzjhd/controller/OrderController.java
+++ src/main/java/com/zjzjhd/controller/OrderController.java
@@ -9,6 +9,7 @@ import com.zjzjhd.entity.OrderDetail;
 import com.zjzjhd.entity.Orders;
 import com.zjzjhd.service.OrderDetailService;
 import com.zjzjhd.service.OrderService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -38,16 +39,17 @@ public class OrderController {
 
     @GetMapping("/userPage")
     public R<Page> page(@RequestParam int page, int pageSize) {
+        //分页构造器对象
         Page<Orders> pageInfo = new Page<>(page, pageSize);
         Page<OrderDto> pageDto = new Page<>(page, pageSize);
-
+        //构造条件查询对象
         LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
         //这里是直接把当前用户分页的全部结果查询出来，要添加用户id作为查询条件，否则会出现用户可以查询到其他用户的订单情况
         queryWrapper.eq(Orders::getUserId, BaseContext.getCurrentId());
         //添加排序条件，根据更新时间降序排列
         queryWrapper.orderByDesc(Orders::getOrderTime);
-
-
+        //分页查询
+        orderService.page(pageInfo, queryWrapper);
 
         //对OrderDto进行需要的属性赋值
         List<Orders> records = pageInfo.getRecords();
@@ -55,9 +57,9 @@ public class OrderController {
             OrderDto orderDto = new OrderDto();
 
             //此时的orderDto对象里面orderDetails属性还是空 下面准备为它赋值
-            Long orderId = item.getId();//获取订单id
+            Long orderId = Long.valueOf(item.getNumber());//获取订单id
 
-            List<OrderDetail> orderDetailList = this.getOrderDetailListByOrderId(orderId);
+            List<OrderDetail> orderDetailList = getOrderDetailListByOrderId(orderId);
 
             BeanUtils.copyProperties(item, orderDto);
 
@@ -67,8 +69,9 @@ public class OrderController {
             return orderDto;
         }).collect(Collectors.toList());
 
-        orderService.page(pageInfo, queryWrapper);
-        return R.success(pageInfo);
+        BeanUtils.copyProperties(pageInfo, pageDto, "records");
+        pageDto.setRecords(orderDtoList);
+        return R.success(pageDto);
     }
 
     private List<OrderDetail> getOrderDetailListByOrderId(Long orderId) {
@@ -76,7 +79,39 @@ public class OrderController {
         queryWrapper.eq(OrderDetail::getOrderId, orderId);
         List<OrderDetail> orderDetailList = orderDetailService.list(queryWrapper);
         return orderDetailList;
+    }
 
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String number, String beginTime, String endTime) {
+        //分页构造器对象
+        Page<Orders> pageInfo = new Page<>(page, pageSize);
+        //构造条件查询对象
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
+
+        //添加查询条件  动态sql  字符串使用StringUtils.isNotEmpty这个方法来判断
+        //这里使用了范围查询的动态SQL，这里是重点！！！
+        queryWrapper.like(number != null, Orders::getNumber, number)
+                .gt(StringUtils.isNotEmpty(beginTime), Orders::getOrderTime, beginTime)
+                .lt(StringUtils.isNotEmpty(endTime), Orders::getOrderTime, endTime);
+
+        orderService.page(pageInfo, queryWrapper);
+        return R.success(pageInfo);
+    }
+
+    /**
+     * @description: 订单派送，订单完成操作 在后端进行操作
+     * @param: [orders]
+     * @return: R<Orders>
+     */
+    @PutMapping
+    public R<Orders> dispatch(@RequestBody Orders orders) {
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(orders.getId() != null, Orders::getId, orders.getId());
+        Orders one = orderService.getOne(queryWrapper);
+
+        one.setStatus(orders.getStatus());
+        orderService.updateById(one);
+        return R.success(one);
     }
 
 }
